0e7bd26f6cb5e4f559ad207169a55c84c8ff812e
ThiAABrown <antunesabrown@gmail.com>
2022-04-14 18:44:57 -0300

    Primeira versão do programa validar cpf

---
+++ scriptspy/verificar_cpf.py
@@ -0,0 +1,34 @@
+def validar_cpf():
+    cpf = list(map(int, input("Digite seu CPF: ")))
+    
+    cpf_vali = cpf[0:9]
+
+    soma1 = 0 
+    for pos, numero in enumerate(cpf_vali):
+       
+        mult = numero * (pos + 1)
+        soma1 = soma1 + mult
+
+        primeiro_digito = round(soma1 % 11)
+
+    if primeiro_digito != cpf[9]:
+        print("CPF INVÁLIDO")
+                
+    else:
+        cpf_vali.append(primeiro_digito)
+        
+    soma2 = 0
+    for pos, numero in enumerate(cpf_vali):
+
+        mult = numero * pos
+        soma2 = soma2 + mult
+        segundo_digito = round(soma2 % 11)
+
+    if segundo_digito != cpf[10]:
+        print("CPF INVÁLIDO")
+    else:
+        cpf_vali.append(segundo_digito)
+        print("CPF VÁLIDO")
+
+
+validar_cpf()
